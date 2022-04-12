bee9e82524dd6311fbb473ea49484b8ee4465bc0
unknown <k.planeta20@gmail.com>
2022-04-12 20:28:52 +0200

    MATI

--- hello.java
+++ hello.java
@@ -1,6 +1,10 @@
 public class hello {
     public static void main(String[] args){
     System.out.println("Hello word");
+
+    for(int mati =0; mati <=blowjob; mati++){
+        
+    }
     }
 
 }
