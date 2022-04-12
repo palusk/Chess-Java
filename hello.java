0a4a240246965c7483e875f4c09f842985938a26
unknown <k.planeta20@gmail.com>
2022-04-12 20:37:07 +0200

    Revert "MATI"
    This reverts commit bee9e82524dd6311fbb473ea49484b8ee4465bc0.

--- hello.java
+++ hello.java
@@ -1,10 +1,6 @@
 public class hello {
     public static void main(String[] args){
     System.out.println("Hello word");
-
-    for(int mati =0; mati <=blowjob; mati++){
-        
-    }
     }
 
 }
